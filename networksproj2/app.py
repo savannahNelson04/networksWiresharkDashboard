6b80314460bddaf3ac743da3506c4b002252c3a3
savannahNelson04 <167340754+savannahNelson04@users.noreply.github.com>
2025-12-11 23:54:28 -0600

    added filed via upload

---
+++ networksproj2/app.py
@@ -0,0 +1,14 @@
+# this file is going to be used for the Flask to display the results of our file
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
